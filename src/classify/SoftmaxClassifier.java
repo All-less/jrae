953eb672d72723138d7adf2d5412db52ace52179
Loy <puregithub@yeah.net>
2019-02-01 21:13:05 +0800

    remove several unnecessary logs

--- src/classify/SoftmaxClassifier.java
+++ src/classify/SoftmaxClassifier.java
@@ -13,11 +13,11 @@ import util.DoubleMatrixFunctions;
 
 /**
  * TODO Make it more generic later
- * Currently works only for <Double,L>, type-casts inside 
- * that may go wrong if you try with something else. 
+ * Currently works only for <Double,L>, type-casts inside
+ * that may go wrong if you try with something else.
  * @author ssanjeev
  */
-public class SoftmaxClassifier<F,L> 
+public class SoftmaxClassifier<F,L>
 	implements ProbabilisticClassifier<F,L>, Serializable{
 
 	private static final long serialVersionUID = 3043693828283322518L;
@@ -27,61 +27,61 @@ public class SoftmaxClassifier<F,L>
 	private DoubleMatrix trainScores, testScores;
 	private SoftmaxCost CostFunction;
 	int CatSize;
-	
+
 	ClassifierTheta ClassifierTheta;
 	Minimizer<DifferentiableFunction> minFunc;
-	
+
 	Accuracy TrainAccuracy, TestAccuracy;
-	
+
 	public SoftmaxClassifier( )
 	{
 		LabelSet = new Counter<L>();
 		minFunc = new QNMinimizer(10,MaxIterations);
 	}
-	
+
 	public SoftmaxClassifier(ClassifierTheta ClassifierParams, Set<L> labelSet)
 	{
 		CatSize = 0;
-		
+
 		LabelSet = new Counter<L>();
 		minFunc = new QNMinimizer(10,MaxIterations);
 		for(L label : labelSet)
 			LabelSet.setCount(label, CatSize++);
-		
+
 		ClassifierTheta = ClassifierParams;
 		assert CatSize == ClassifierTheta.CatSize;
 	}
-	
+
 	public DoubleMatrix getTrainingResults(List<LabeledDatum<F,L>> Data)
 	{
 		populateLabels(Data);
 		DoubleMatrix Features = makeFeatureMatrix(Data);
 		int FeatureLength = Features.rows;
-		
+
 		int[] Labels = makeLabelVector(Data);
 		CostFunction = new SoftmaxCost(Features,Labels,CatSize,Lambda);
-		
+
 		ClassifierTheta = new ClassifierTheta(FeatureLength,CatSize);
 		double[] InitialTheta = ClassifierTheta.Theta;
-		
+
 		double[] OptimalTheta = minFunc.minimize(CostFunction, 1e-6, InitialTheta);
 		ClassifierTheta = new ClassifierTheta(OptimalTheta,FeatureLength,CatSize);
 		return CostFunction.getPredictions(ClassifierTheta, Features);
 	}
-	
+
 	public Accuracy train(List<LabeledDatum<F,L>> Data)
-	{	
+	{
 		trainScores = getTrainingResults(Data);
 		int[] GTLabels = makeLabelVector(Data);
 		int[] Predictions = trainScores.columnArgmaxs();
 		TrainAccuracy = new Accuracy(Predictions, GTLabels, CatSize);
 		return TrainAccuracy;
 	}
-	
+
 	public Accuracy test(List<LabeledDatum<F,L>> Data)
 	{
 		DoubleMatrix Features = makeFeatureMatrix(Data);
-		DoubleMatrixFunctions.prettyPrint(Features);
+		// DoubleMatrixFunctions.prettyPrint(Features);
 		int[] Labels = makeLabelVector(Data);
 		CostFunction = new SoftmaxCost (CatSize, ClassifierTheta.FeatureLength, Lambda);
 		testScores = CostFunction.getPredictions(ClassifierTheta,Features);
@@ -89,21 +89,21 @@ public class SoftmaxClassifier<F,L>
 		TestAccuracy = new Accuracy(Predictions,Labels,CatSize);
 		return TestAccuracy;
 	}
-	
+
 	public DoubleMatrix getTrainScores()
 	{
 		if (trainScores == null)
 			System.err.println("Train scores polled before training! Will be null.");
 		return trainScores;
 	}
-	
+
 	public DoubleMatrix getTestScores()
 	{
 		if (testScores == null)
 			System.err.println("Test scores polled before training! Will be null.");
 		return testScores;
 	}
-	
+
 	private void populateLabels(List<LabeledDatum<F,L>> Data)
 	{
 		CatSize = 0;
@@ -118,7 +118,7 @@ public class SoftmaxClassifier<F,L>
 		}
 		CatSize = LabelSet.keySet().size();
 	}
-	
+
 	private int[] makeLabelVector(List<LabeledDatum<F,L>> Data)
 	{
 		int[] Labels = new int[ Data.size() ];
@@ -127,13 +127,13 @@ public class SoftmaxClassifier<F,L>
 			Labels[i++] = (int) LabelSet.getCount( datum.getLabel() );
 		return Labels;
 	}
-	
+
 	private DoubleMatrix makeFeatureMatrix(List<LabeledDatum<F,L>> Data)
 	{
 		int NumExamples = Data.size();
 		if( NumExamples == 0 )
 			return null;
-		
+
 		int FeatureLength = Data.get(0).getFeatures().size();
 		double[][] features = new double[FeatureLength][NumExamples];
 		for(int i=0; i<NumExamples; i++)
@@ -148,32 +148,32 @@ public class SoftmaxClassifier<F,L>
 		}
 		return new DoubleMatrix(features);
 	}
-	
+
 	@Override
-	public L getLabel(Datum<F> datum) 
+	public L getLabel(Datum<F> datum)
 	{
 		Counter<L> probabilities = getProbabilities(datum);
 		return probabilities.argMax();
 	}
 
 	@Override
-	public Counter<L> getProbabilities(Datum<F> datum) 
+	public Counter<L> getProbabilities(Datum<F> datum)
 	{
 		Collection<F> f = datum.getFeatures();
 		Counter<L> probabilities = new Counter<L>();
 		DoubleMatrix Features = DoubleMatrix.zeros(ClassifierTheta.FeatureLength, 1);
 		int i=0;
 		for(F feature : f)
-		{		
+		{
 			Features.put(i, 0, (Double) feature);
 			i++;
 		}
-		
+
 		if (CostFunction == null)
 			CostFunction = new SoftmaxCost (CatSize, ClassifierTheta.FeatureLength, Lambda);
-		
+
 		DoubleMatrix Scores = CostFunction.getPredictions(ClassifierTheta, Features);
-		
+
 		for(L label : LabelSet.keySet())
 		{
 			int labelIndex = (int) LabelSet.getCount(label);
@@ -187,13 +187,13 @@ public class SoftmaxClassifier<F,L>
 		Counter<L> probablities = getProbabilities(datum);
 		Counter<L> logProbablities = new Counter<L>();
 		for(L label : probablities.keySet())
-		{	
+		{
 			double logProb = Math.log( probablities.getCount(label) );
 			logProbablities.setCount(label, logProb);
 		}
 		return logProbablities;
 	}
-	
+
 	public void Dump (String fileName) throws IOException{
 		FileOutputStream fos = new FileOutputStream(fileName);
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
--- src/io/ParsedReviewData.java
+++ src/io/ParsedReviewData.java
@@ -15,20 +15,20 @@ import classify.LabeledDatum;
 import classify.ReviewDatum;
 
 /*
- * Routines for reading in the review data and processing them into Vocab and 
+ * Routines for reading in the review data and processing them into Vocab and
  * vectors into Vocab (Words_Index) for words in each sentence (Sentence_Words)
- * 
+ *
  * @author Sanjeev Satheesh, Daniel Seng
  */
 public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Integer>,Integer,Integer>
-{ 
+{
 	private int NumExamples, NumTestExamples, counter, testCounter;
 	private int[] Sentence_lengths;
 	private WordMap WordsIndexer;
 	private StringLabelSet labelMapping;
 	private boolean testLabelsKnown;
 	private RobustTokenizer<Word> tokenizer;
-	
+
 	public ParsedReviewData()
 	{
 		counter = 0;
@@ -36,27 +36,27 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 		WordsIndexer = new WordMap();
 		labelMapping = new StringLabelSet();
 	}
-	
+
 	public ParsedReviewData(int minCount)
 	{
 		this();
 		MINCOUNT = minCount;
 	}
-	
-	public ParsedReviewData(String DirectoryName, int minCount, 
+
+	public ParsedReviewData(String DirectoryName, int minCount,
 			String wordmapFileName, String labelsetFileName) throws IOException{
 		this(minCount);
-		
+
 		File Dir = new File(DirectoryName);
 		File labelSetFile = null, wordMapFile = null;
 		String[] files = Dir.list( );
-		
+
 		wordMapFile = (wordmapFileName == null)?
 				new File (DirectoryName,"wordmap.map"): new File (wordmapFileName);
 
 		labelSetFile = (labelsetFileName == null)?
 				new File (DirectoryName,"labels.map"): new File (labelsetFileName);
-		
+
 		NumExamples = 0;
 		NumTestExamples = 0;
 		for(String fileName : files )
@@ -68,57 +68,57 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 				else
 					NumExamples += IOUtils.countLines(FullFileName);
 		}
-		
+
 		if (NumExamples == 0 && (!wordMapFile.exists () || !labelSetFile.exists ()) )
 			throw new IOException ("If you are not training, then please provide both " +
 									"the labels.map and wordmap.map files as input. ");
-		
+
 		Sentence_lengths = new int[ NumExamples ];
 		Data = new ArrayList<LabeledDatum<Integer,Integer>>( NumExamples );
 		TestData = new ArrayList<LabeledDatum<Integer,Integer>>( NumTestExamples );
-		
-		System.out.println(NumTestExamples + " " + NumExamples);
-		
+
+		// System.out.println(NumTestExamples + " " + NumExamples);
+
 		Arrays.sort(files);
 		LoadTrainingData(Dir, files);
-		
+
 		if (labelSetFile.exists())
 			labelMapping = new StringLabelSet (labelSetFile.getAbsolutePath());
 		else
 		{
 			labelMapping.saveToFile(labelSetFile.getAbsolutePath());
-		}	
-		
+		}
+
 		LoadTestData(Dir, files);
-		
+
 		if (wordMapFile.exists())
-			WordsIndexer = new WordMap (wordMapFile.getAbsolutePath());			
+			WordsIndexer = new WordMap (wordMapFile.getAbsolutePath());
 		else
 		{
 			WordsIndexer = new WordMap (Vocab, MINCOUNT);
 			WordsIndexer.saveToFile(wordMapFile.getAbsolutePath());
 		}
-		
+
 		EmbedWords();
-		
+
 		printComplete();
 	}
-	
+
 	private void printComplete()
 	{
 		System.out.println("Data has been completely loaded.\n"
 			+"There are " + counter + " training examples and "+testCounter+ " test examples.\n"
 			+"There are a total of "+WordsIndexer.size()+" elements in the vocabulary."
 			+"Note that we only count words that occur atleast "+ MINCOUNT +" number of times. \n"
-			+"We maintain an internal mapping of String labels to integers. " 
+			+"We maintain an internal mapping of String labels to integers. "
 			+"Class probabilities output are in increasing order of this mapping, and it is as follows\n\n{");
-		
+
 		for(int label=0; label < labelSet.size(); label++)
 			System.out.println("\t" + label + " : " + getLabelString(label));
-		
+
 		System.out.println("}");
 	}
-	
+
 	private void LoadTrainingData (File Dir, String[] files) throws IOException
 	{
 		int CurrentLabel = 0;
@@ -133,9 +133,9 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 				labelMapping.put(getLabelString(fileName),CurrentLabel);
 				CurrentLabel++;
 			}
-		}	
+		}
 	}
-	
+
 	private void LoadTestData (File Dir, String[] files) throws IOException
 	{
 		int CurrentLabel = 0;
@@ -150,16 +150,16 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 				LoadTestFile(FullFileName,CurrentLabel);
 			}
 		}
-		
+
 		if (testLabelsSeen.size () > 1)
 			testLabelsKnown = true;
-		
+
 	}
-	
+
 	private void LoadTrainFile(String FileName, int Label) throws IOException
 	{
 		assert Label != ReviewDatum.UnknownLabel;
-		
+
 		BufferedReader inBr = new BufferedReader(new InputStreamReader(
     new FileInputStream(FileName), "UTF-8"));
 		int iCountLines = 0;
@@ -175,21 +175,21 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 			// }
 
 			Word[] words = list.toArray(new Word[list.size()]);
-			
+
 			if (words == null || words.length == 0)
 				continue;
 			Data.add( new ReviewDatum(words, Label, counter) );
 			for (Word word : words)
 				Vocab.addKey( word.word() );
-			
+
 			Sentence_lengths[counter] = words.length;
-			counter++;	
+			counter++;
 			iCountLines++;
 		}
 		inBr.close();
 		System.out.println(iCountLines+" train examples read from " + FileName);
 	}
-	
+
 	private void LoadTestFile(String FileName, int Label) throws IOException
 	{
 		BufferedReader inBr = new BufferedReader(new InputStreamReader(
@@ -203,90 +203,90 @@ public class ParsedReviewData extends LabeledDataSet<LabeledDatum<Integer,Intege
 			sLine = sLine.trim();
 
 			ptbt = new PTBTokenizer(new StringReader(sLine), new WordTokenFactory(), "");
-			
+
 			// Alternatively, could use robust tokenizer for English or some other tokenizer
 			// for languages with scripts like Chinese or Arabic
 			// tokenizer = new RobustTokenizer<Word>(sLine);
 			List<Word> list = ptbt.tokenize();
 
 			Word[] words = list.toArray(new Word[list.size()]);
-			
+
 			if (words == null || words.length == 0)
 				continue;
-			
+
 			TestData.add( new ReviewDatum(words, Label, testCounter) );
 			testCounter++;
 			iCountLines++;
 		}
 		inBr.close();
-		System.out.println(iCountLines+" test examples read from " + FileName);		
+		System.out.println(iCountLines+" test examples read from " + FileName);
 	}
-	
+
 	protected void EmbedWords()
 	{
 		Vocab = WordsIndexer.getVocab();
-		
-		for (LabeledDatum<Integer,Integer> DataItem : Data) 
+
+		for (LabeledDatum<Integer,Integer> DataItem : Data)
 			((ReviewDatum)DataItem).indexWords(WordsIndexer);
 
-		for (Datum<Integer> DataItem : TestData) 
+		for (Datum<Integer> DataItem : TestData)
 			((ReviewDatum)DataItem).indexWords(WordsIndexer);
 	}
-	
+
 	protected int getTestLabel(String fileName)
 	{
-		assert fileName.length() > 8; //test/_.+/.txt 
+		assert fileName.length() > 8; //test/_.+/.txt
 		assert isTestDataFile(fileName);
-		
+
 		String labelName = getLabelString (fileName);
 		return getLabel (labelName);
 	}
-	
+
 	protected boolean isFile(String fileName){
 		return (new File(fileName).isFile() && fileName.endsWith(".txt"));
 	}
-	
+
 	protected boolean isTestDataFile(String fileName){
 		String baseName = new File(fileName).getName();
 		return baseName.endsWith(".txt") && baseName.indexOf("test") == 0;
 	}
-	
+
 	protected String getLabelString (String str)
 	{
 		String baseName = str;
-		
+
 		// Could be a filename
 		baseName = new File(str).getName();
 		int extensionIndex = baseName.indexOf(".txt");
 
 		// better not be an empty label name
-		if ( extensionIndex > 0) 
+		if ( extensionIndex > 0)
 			baseName = baseName.substring(0, extensionIndex);
-		
+
 		if(baseName.indexOf("test") == 0)
 			baseName = baseName.substring(4);
-		
+
 		if(baseName.length() > 0 && baseName.charAt(0) == '_')
 			baseName = baseName.substring(1);
-		
+
 		return baseName;
 	}
-	
+
 	public boolean isTestLablesKnown()
 	{
 		return testLabelsKnown;
 	}
-	
+
 	public int getLabel(String label)
 	{
 		return labelMapping.getLabel(label);
 	}
-	
+
 	public String getLabelString (int label)
 	{
 		return labelMapping.getLabelString(label);
 	}
-	
+
 	public int getWordIndex(String Word)
 	{
 		return WordsIndexer.getMapping(Word);
